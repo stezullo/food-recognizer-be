2e297c73bf1918f56e4ec1192aee44b9d3ed234d
Stefano <ste.zullo@icloud.com>
2019-08-07 23:41:29 +0100

    Recognition Service Alpha

--- src/controllers/RecognitionController.ts
+++ src/controllers/RecognitionController.ts
@@ -5,15 +5,24 @@ import { Request, Response } from "express";
 export class RecognitionController {
     static recognition = async (req: Request, res: Response) => {
 
-        let recognitionResult: any;
+        let { photo } = req.body;
+
+        if (!photo) {
+            // Bad request
+            res.status(400).send();
+            return;
+        }
+
+        let photoBuffer: Buffer = Buffer.from(photo);
+
         // Creates a client
         const client = new vision.ImageAnnotatorClient();
 
         // Performs label detection on the image file
-        const [result] = await client.labelDetection("./src/resources/pizza.jpg");
-        const labels = result.labelAnnotations;
+        const [result] = await client.labelDetection(photoBuffer);
+        const recognitionResult = result.labelAnnotations;
 
-        res.status(200).send(labels);
+        res.status(200).send(recognitionResult);
     }
 }
 
