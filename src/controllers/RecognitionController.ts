637b72bcba40c3d0df54f047361c65d7c8f9f9dd
Stefano <ste.zullo@icloud.com>
2019-09-05 16:00:36 +0100

    da

--- src/controllers/RecognitionController.ts
+++ src/controllers/RecognitionController.ts
@@ -4,7 +4,9 @@ import vision = require('@google-cloud/vision');
 
 export class RecognitionController {
     static recognition = async (req: Request, res: Response) => {
-
+        let currentPath = process.cwd();
+        console.log("Current directory");
+        console.log(currentPath);
         let file = req.file;
 
         if (!file) {
